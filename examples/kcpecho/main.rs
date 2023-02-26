7d2794f9bf5462b0fa4d6dcab6e996c50ad9c066
luyikk <luyikk@126.com>
2023-02-26 20:16:29 +0800

    lints

--- examples/kcpecho/main.rs
+++ examples/kcpecho/main.rs
@@ -1,3 +1,4 @@
+use std::error::Error;
 use kcpserver::prelude::{
     kcp_module::{KcpConfig, KcpNoDelayConfig},
     *,
@@ -9,18 +10,20 @@ async fn main() -> anyhow::Result<()> {
     env_logger::Builder::new()
         .filter_level(LevelFilter::Debug)
         .init();
-
     let mut config = KcpConfig::default();
     config.nodelay = Some(KcpNoDelayConfig::fastest());
-    let kcp_server = KcpListener::new("0.0.0.0:5555", config, 5, |peer| async move {
-        log::debug!("create kcp peer:{}", peer.to_string());
-        let mut buf = [0; 1024];
-        while let Ok(size) = peer.recv(&mut buf).await {
-            log::debug!("read peer:{} buff:{}", peer.to_string(), size);
-            peer.send(&buf[..size]).await?;
-        }
-        Ok(())
-    })?;
-    kcp_server.start().await?;
+    KcpListener::new("0.0.0.0:5555", config, 5, input)?
+        .start().await?;
+    Ok(())
+}
+
+#[inline]
+async fn input(peer:KCPPeer)->Result<(), Box<dyn Error>>{
+    log::debug!("create kcp peer:{}", peer.to_string());
+    let mut buf = [0; 1024];
+    while let Ok(size) = peer.recv(&mut buf).await {
+        log::debug!("read peer:{} buff:{}", peer.to_string(), size);
+        peer.send(&buf[..size]).await?;
+    }
     Ok(())
 }
